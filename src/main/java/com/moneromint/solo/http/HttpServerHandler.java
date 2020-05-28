b7c1c0ec28f185dbd098f6e13349f99d969ef262
Matt Smith <matt@offtopica.uk>
2020-05-28 11:01:12 +0100

    http: Add /stats.json endpoint
    Also allow HEAD requests for CORS pre-flight checks.

--- src/main/java/com/moneromint/solo/http/HttpServerHandler.java
+++ src/main/java/com/moneromint/solo/http/HttpServerHandler.java
@@ -1,7 +1,10 @@
 package com.moneromint.solo.http;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moneromint.solo.BlockTemplateUpdater;
 import com.moneromint.solo.GlobalStats;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufOutputStream;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -10,8 +13,13 @@ import io.netty.util.CharsetUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Map;
+
 public class HttpServerHandler extends ChannelInboundHandlerAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpServerHandler.class);
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     private final BlockTemplateUpdater blockTemplateUpdater;
     private final GlobalStats globalStats;
@@ -22,46 +30,55 @@ public class HttpServerHandler extends ChannelInboundHandlerAdapter {
     }
 
     private HttpResponse handleFetchTemplate(HttpRequest request) {
-        blockTemplateUpdater.update();
+        if (request.method() == HttpMethod.GET) {
+            blockTemplateUpdater.update();
+        }
         return new DefaultFullHttpResponse(request.protocolVersion(), HttpResponseStatus.OK);
     }
 
     private HttpResponse handleMetrics(HttpRequest request) {
-        String body =
-                // Write blocks found.
-                "# HELP stratum_blocks_found Total number of blocks found by the pool.\n" +
-                        "# TYPE stratum_blocks_found counter\n" +
-                        "stratum_blocks_found " +
-                        globalStats.getBlocksFound() +
-                        "\n\n" +
+        final ByteBuf buf;
 
-                        // Write connection count.
-                        "# HELP stratum_connections_count The total number of connections to the stratum server.\n" +
-                        "# TYPE stratum_connections_count gauge\n" +
-                        "stratum_connections_count " +
-                        globalStats.getConnectionCount() +
-                        "\n\n" +
+        if (request.method() == HttpMethod.GET) {
+            final String body =
+                    // Write blocks found.
+                    "# HELP stratum_blocks_found Total number of blocks found by the pool.\n" +
+                            "# TYPE stratum_blocks_found counter\n" +
+                            "stratum_blocks_found " +
+                            globalStats.getBlocksFound() +
+                            "\n\n" +
 
-                        // Write share count.
-                        "# HELP stratum_shares_total The total number of shares submitted to the server.\n" +
-                        "# TYPE stratum_shares_total counter\n" +
-                        "stratum_shares_total{valid=\"true\"} " +
-                        globalStats.getValidShares() +
-                        '\n' +
-                        "stratum_shares_total{valid=\"false\"} " +
-                        globalStats.getInvalidShares() +
-                        "\n\n" +
+                            // Write connection count.
+                            "# HELP stratum_connections_count The total number of connections to the stratum server" +
+                            ".\n" +
+                            "# TYPE stratum_connections_count gauge\n" +
+                            "stratum_connections_count " +
+                            globalStats.getConnectionCount() +
+                            "\n\n" +
 
-                        // Write total hashes.
-                        // It is better to send the number of hashes and letting the database work out the hashrate,
-                        // instead
-                        // of the pool working out the hashrate and sending it.
-                        "# HELP stratum_hashes_total The total number of hashes submitted to the server.\n" +
-                        "# TYPE stratum_hashes_total counter\n" +
-                        "stratum_hashes_total " +
-                        globalStats.getTotalHashes().toString(10) +
-                        '\n';
-        final var buf = Unpooled.wrappedBuffer(body.getBytes(CharsetUtil.UTF_8));
+                            // Write share count.
+                            "# HELP stratum_shares_total The total number of shares submitted to the server.\n" +
+                            "# TYPE stratum_shares_total counter\n" +
+                            "stratum_shares_total{valid=\"true\"} " +
+                            globalStats.getValidShares() +
+                            '\n' +
+                            "stratum_shares_total{valid=\"false\"} " +
+                            globalStats.getInvalidShares() +
+                            "\n\n" +
+
+                            // Write total hashes.
+                            // It is better to send the number of hashes and letting the database work out the hashrate,
+                            // instead of the pool working out the hashrate and sending it.
+                            "# HELP stratum_hashes_total The total number of hashes submitted to the server.\n" +
+                            "# TYPE stratum_hashes_total counter\n" +
+                            "stratum_hashes_total " +
+                            globalStats.getTotalHashes().toString(10) +
+                            '\n';
+
+            buf = Unpooled.wrappedBuffer(body.getBytes(CharsetUtil.UTF_8));
+        } else {
+            buf = Unpooled.buffer(0);
+        }
 
         final var response = new DefaultFullHttpResponse(request.protocolVersion(), HttpResponseStatus.OK, buf);
 
@@ -70,14 +87,41 @@ public class HttpServerHandler extends ChannelInboundHandlerAdapter {
         return response;
     }
 
-    private HttpResponse handleRequest(HttpRequest request) {
-        if (request.method() != HttpMethod.GET) {
+    private HttpResponse handleStatsJson(HttpRequest request) throws IOException {
+        final ByteBuf buf;
+
+        if (request.method() == HttpMethod.GET) {
+            final var content = Map.of(
+                    "connections", globalStats.getConnectionCount(),
+                    "hashrate", globalStats.estimateHashrate().longValue(),
+                    "validShares", globalStats.getValidShares(),
+                    "invalidShares", globalStats.getInvalidShares(),
+                    "totalHashes", globalStats.getTotalHashes().toString()
+            );
+
+            buf = Unpooled.buffer();
+            final DataOutput dataOutput = new ByteBufOutputStream(buf);
+            MAPPER.writeValue(dataOutput, content);
+        } else {
+            buf = Unpooled.buffer(0);
+        }
+
+        final var response = new DefaultFullHttpResponse(request.protocolVersion(), HttpResponseStatus.OK, buf);
+        response.headers().set("Content-Type", "application/json");
+        response.headers().set("Access-Control-Allow-Origin", "*"); // Allow access from any domain
+
+        return response;
+    }
+
+    private HttpResponse handleRequest(HttpRequest request) throws IOException {
+        if (request.method() != HttpMethod.GET && request.method() != HttpMethod.HEAD) {
             return new DefaultFullHttpResponse(request.protocolVersion(), HttpResponseStatus.METHOD_NOT_ALLOWED);
         }
 
         return switch (request.uri()) {
             case "/-/fetchtemplate" -> handleFetchTemplate(request);
             case "/metrics" -> handleMetrics(request);
+            case "/stats.json" -> handleStatsJson(request);
             default -> new DefaultFullHttpResponse(request.protocolVersion(), HttpResponseStatus.NOT_FOUND);
         };
     }
